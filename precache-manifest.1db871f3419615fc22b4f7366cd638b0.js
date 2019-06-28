52ef82cbaa59ce03e4e46e15633b3f47592034da
meller96 <meller70@gmail.com>
2019-06-28 03:45:57 +0300

    Updates

---
+++ precache-manifest.1db871f3419615fc22b4f7366cd638b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df6017f44293d1490f35e44402d6f84",
+    "url": "/weatherApplication/index.html"
+  },
+  {
+    "revision": "9bc6c79da7ef834a16eb",
+    "url": "/weatherApplication/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0beb626f2400488b9ace",
+    "url": "/weatherApplication/static/css/main.1957e60d.chunk.css"
+  },
+  {
+    "revision": "9bc6c79da7ef834a16eb",
+    "url": "/weatherApplication/static/js/2.e597a5a4.chunk.js"
+  },
+  {
+    "revision": "0beb626f2400488b9ace",
+    "url": "/weatherApplication/static/js/main.2766b352.chunk.js"
+  },
+  {
+    "revision": "050ef84c023c99a26b1a",
+    "url": "/weatherApplication/static/js/runtime~main.28c62b6a.js"
+  },
+  {
+    "revision": "98b4222f96577b29bec0a053ecc1773e",
+    "url": "/weatherApplication/static/media/bg.98b4222f.jpg"
+  }
+]);
